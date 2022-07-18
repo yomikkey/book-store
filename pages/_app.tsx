59a05725211f5f0a9659548003970276a5526531
yomikkey <anukulmickey9@gmail.com>
2022-07-18 15:21:11 +0530

    few changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useState, useCallback } from 'react';
-import BookStore from './styling-components/bookStore';
-import HeaderStyle from './styling-components/headerStyle';
-import SearchBox from './styling-components/searchBox';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [searchValue, setSearchValue] = useState("");
-  const [searchCategory, setsearchCategory] = useState('title')
-
-  const searchCallback = useCallback((searchValue: any) => {
-    setSearchValue(searchValue);
-  }, []);
-
-  const searchCategoryCallBack = useCallback((searchCategory: any) => {
-    setSearchValue(searchCategory);
-  }, []);
 
   return (
     <>
-      <HeaderStyle />
-      <SearchBox searchCallback={searchCallback} searchCategoryCallBack={searchCategoryCallBack}/>
-      <BookStore searchValue={searchValue} searchCategory={searchCategory} />
+      <div>
+        hi
+      </div>
     </>
   )
 }
